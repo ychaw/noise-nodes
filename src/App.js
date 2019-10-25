f003a08b9481aea3e0d1ee8f5fc66e12b21652c7
Ian F <ianmichael.fennie@haw-hamburg.de>
2019-10-25 07:01:17 +0200

    Initiliaze Footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './style/colors.css';
 import './App.css';
 import Header from './components/layout/Header';
+import Footer from './components/layout/Footer';
 import BaseNode from './modules/BaseNode';
 
 class App extends Component {
@@ -10,6 +11,7 @@ class App extends Component {
       <div className="App">
         <Header />
         <BaseNode />
+        <Footer />
       </div>
     );
   }
---
+++ src/components/layout/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div style={footerStyle}>HAW Hamburg AVPRG W19</div>
+    );
+}
+
+const footerStyle = {
+    padding: '5px',
+    background: '#123649',
+    textAlign: 'right',
+    fontSize: '10px',
+    color: 'white'
+}
+
+export default Footer;
