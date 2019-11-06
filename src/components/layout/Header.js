626e730d830a72a1f5d71c28fb8766fde89a4f94
Ian F <ianmichael.fennie@haw-hamburg.de>
2019-11-06 15:04:23 +0100

    Style header as flexbox model

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -2,28 +2,51 @@ import React from 'react';
 
 function Header() {
     return (
-        <header style={headerStyle}>
-            <h1 style={titleStyle}>NoiseNodes</h1>
-            <h2 style={aboutStyle}>About</h2>
+       <header style={headerStyle}>
+            <div style={headerItemStyle}>
+                <h1>NoiseNodes</h1>
+            </div>
+            <div style={{...headerItemStyle,...palletStyle}}>
+                <h1 style={palletItemStyle}>+</h1>
+            </div>
+            <div style={{...headerItemStyle,...aboutStyle}}>
+                <h2>About</h2>
+            </div>
         </header>
     )
 }
 
 const headerStyle = {
     display: 'flex',
-    background: '#123649',
-    border: '1px solid black'
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    background: 'var(--primary-shade3)',
+    border: '1px solid black',
+    color: 'white',
 }
 
-const titleStyle = {
-    padding: '10px',
-    background: '#042130',
-    color: 'white'
+const headerItemStyle = {
+    flexBasis: '250px',
+    background: 'var(--primary-shade4)',
 }
+
+const palletStyle = {
+    display: 'flex',
+    flexGrow: '1',
+    justifyContent: 'center',
+    alignItems: 'center',
+    background: 'var(--primary-shade3)',
+}
+
+const palletItemStyle = {
+    flexBasis: '50px',
+    background: 'var(--primary-shade2)',
+}
+
 const aboutStyle = {
-    padding: '20px',
-    background: '#042130',
-    color: 'white'
+    // alignSelf: 'stretch',
+    // alignContent: 'center',
+    // flexShrink: '1',
  }
 
 export default Header;
