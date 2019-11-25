80ce12c789244a58f48a459965cee2e1e88a907c
Yannick Clausen <yannick.clausen@haw-hamburg.de>
2019-11-25 13:21:12 +0100

    simplify settings to use only percentages

--- src/modules/Setting.js
+++ src/modules/Setting.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 function Setting(props) {
-    const {name, unit, changeValue, min, max, step, value} = props;
+    //const {name, unit, changeValue, min, max, step, value} = props;
+    const {name, unit, changeValue, value} = props;
     return (
       <div>
         <p>{name}: {value} {unit}</p>
-        <input type='range' min={min} max={max} step={step} value={value} onChange={changeValue}></input>
+        <input type='range' min={0} max={1} step={0.01} value={value} onChange={changeValue}></input>
       </div>
     );
 }
