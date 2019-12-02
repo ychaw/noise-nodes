d8623cda6b134f83d56878f26a97764036c147eb
Yannick Clausen <yannick.clausen@haw-hamburg.de>
2019-12-02 19:44:36 +0100

    visuals for connector

--- src/modules/Connector.js
+++ src/modules/Connector.js
@@ -8,15 +8,48 @@ class Connector extends React.Component {
     }
   }
 
-  render() {
-    const {id, type, audioNode} = this.props;
-    return <button style={style} id='connector-button' onClick={this.props.select.bind(this, id, type, audioNode)}>{type}</button>
+  componentDidMount() {
   }
-}
 
-const style = {
-  padding: '0.7rem',
-  borderRadius: '100%',
+  handleClick = (e) => {
+    const {id, type, audioNode} = this.props;
+    this.props.select(id, type, audioNode);
+  }
+
+  getColors = () => {
+    if(RegExp('audio-.*').test(this.props.type)) {
+      return {stroke: 'var(--secondary1-shade1)', fill: 'var(--secondary1-shade0)'}
+    } else if(RegExp('control-.*').test(this.props.type)) {
+      return {stroke: 'var(--secondary2-shade1)', fill: 'var(--secondary2-shade0)'}
+    }
+  }
+
+  isInput = () => {
+    return RegExp('.-input').test(this.props.type);
+  }
+
+  render() {
+    let height = 100,
+        width = 100,
+        dim = 16,
+        strokeWidth = dim/3 * 2,
+        boundary = dim+strokeWidth,
+        colors = this.getColors(),
+        isInput = this.isInput();
+    return <svg
+      height={height}
+      width={width}>
+      <circle
+         cx={isInput ? boundary : width-boundary}
+         cy={dim+strokeWidth}
+         r={dim}
+         stroke={colors.stroke}
+         strokeWidth={strokeWidth}
+         fill={colors.fill}
+         onClick={this.handleClick}
+       />
+    </svg>
+  }
 }
 
 export default Connector;
