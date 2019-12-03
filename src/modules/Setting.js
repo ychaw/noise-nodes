10d53c214740d3d8350c5a342f867533ff07b808
Ian F <ianmichael.fennie@haw-hamburg.de>
2019-12-03 16:59:32 +0100

    Fix merge error

--- src/modules/Setting.js
+++ src/modules/Setting.js
@@ -1,17 +1,16 @@
 import React from 'react';
 
 function Setting(props) {
-    const {name, unit, value} = props;
-    const changeValue = (e) => {
-      props.changeValue(e, props.target, props.value);
-    }
-    return (
-      <div style={style}>
-        <p>{name}: {Number(Number(value.absValue).toPrecision(2))} {unit}</p>
-        <input type='range' min={0} max={1} step={0.01} value={value.relValue} onChange={changeValue}></input>
-      </div>
-    );
+  const {name, unit, value} = props;
+  const changeValue = (e) => {
+    props.changeValue(e, props.target, props.value);
   }
+  return (
+    <div style={style}>
+      <p>{name}: {Number(Number(value.absValue).toPrecision(2))} {unit}</p>
+      <input type='range' min={0} max={1} step={0.01} value={value.relValue} onChange={changeValue}></input>
+    </div>
+  );
 }
 
 const style = {
